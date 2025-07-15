18e2ebeef39c510a87d117b2c1a96b1047fade0b
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-07-14 22:21:14 -0500

    Fix(frontend.py): Fix IndexError, when the user click the listbox

--- frontend.py
+++ frontend.py
@@ -19,20 +19,22 @@ import backend
 
 def get_selected_row(event):
     global selected_tuple
-    index=list1.curselection()[0]
-    selected_tuple=list1.get(index)
-    e1.delete(0, END)
-    e1.insert(END, selected_tuple[1])
+    if list1.curselection(): 
+        index=list1.curselection()
+        selected_tuple=list1.get(index)
+        e1.delete(0, END)
+        e1.insert(END, selected_tuple[1])
+        
+        e2.delete(0, END)
+        e2.insert(END, selected_tuple[2])
+        
+        e3.delete(0, END)
+        e3.insert(END, selected_tuple[3])
+        
+        e4.delete(0, END)
+        e4.insert(END, selected_tuple[4])
     
-    e2.delete(0, END)
-    e2.insert(END, selected_tuple[2])
-    
-    e3.delete(0, END)
-    e3.insert(END, selected_tuple[3])
-    
-    e4.delete(0, END)
-    e4.insert(END, selected_tuple[4])
-    
+        
 def view_command():
     list1.delete(0, END)
     for row in backend.view():
